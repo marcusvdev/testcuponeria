25b84b1297ba53a118e7c98111dded886c5841f8
Marcus Vinicius <marcusvdev@gmail.com>
2022-02-15 09:51:55 -0300

    Finished the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/link-passhref */
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from './footer.module.css'
@@ -6,7 +7,28 @@ export default function Footer() {
 
     return (
         <footer className={styles.footer}>
-            footer
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/Cuponeria/" target="_blank" rel="noreferrer">
+                        <Image src="/facebook.png" alt="Facebook" width={39} height={39} />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/cuponeria" target="_blank" rel="noreferrer">
+                        <Image src="/instagram.png" alt="Instagram" width={39} height={39} />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/Cuponeria" target="_blank" rel="noreferrer">
+                        <Image src="/twitter.png" alt="Twitter" width={39} height={39} />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/channel/UCvdHfsvEleYAGYOhIzPwNGg" target="_blank" rel="noreferrer">
+                        <Image src="/youtube.png" alt="Youtube" width={39} height={39} />
+                    </a>
+                </li>
+            </ul>
         </footer>
     )
 }
